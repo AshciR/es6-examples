2c6bc36e7a957e5819aba70980fd113c24cd09da
Richard Walker <richard.walker.90@gmail.com>
2017-06-12 21:47:08 -0400

    inital commit

---
+++ var-let-const-es5.js
@@ -0,0 +1,25 @@
+// In ES5 there is only one type of variable scope; function scope
+for (var i = 0; i < 10; i++){
+  console.log(i)
+}
+console.log('after loop', i);
+
+/* The code block below shows an example of variables
+being function scoped. If you move line 14
+after the counter() call you'll get a runtime error. */
+// function counter(){
+//   for (var i = 0; i < 10; i++){
+//     console.log(i)
+//   }
+//   console.log('after loop', i);
+// }
+// counter()
+
+/* To counteract the scoping problem people would
+use immideately invoked function expressions (IIFEs) */
+// (function (){
+//   for (var i = 0; i < 10; i++){
+//     console.log(i)
+//   }
+//   console.log('after loop', i);
+// })()
