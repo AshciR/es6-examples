dbe92455fe39d46c58df4b2acfa3ed4a3090e8c4
Richard Walker <richard.walker.90@gmail.com>
2017-06-13 10:41:40 -0400

    finished the arrow examples for es5

--- arrow-functions/funct-es5.js
+++ arrow-functions/funct-es5.js
@@ -1,22 +1,34 @@
-// let allows reassignment after variable creation
-let x = 1
-x = 2
-console.log(x)
+const vacationSpots = [
+  {name: "Jamaica", cost: 500.00, climate: "Warm"},
+  {name: "Ireland", cost: 400.00, climate: "Mild"},
+  {name: "Maldives",   cost: 1000.00, climate: "Warm"},
+  {name: "Alaska", cost: 800.00, climate: "Cold"},
+  {name: "Rome", cost: 700.00, climate: "Mild"}
+]
+console.log(vacationSpots);
 
-// const does not allow reassignment.
-// if you're familiar with Java, consider it as equivalent of 'final' variable
-// const y = 1
-// y = 2 // this is going to through a RTE because y value can't be reassigned
-// console.log(y)
+// Let's an array of the vacation spots that
+// are less than $500 and mild climate
+const mildCheapSpots = vacationSpots
+  .filter(function(spots){
+    return spots.climate === "Mild"
+  })
+  .filter(function(spots){
+    return spots.cost <= 500.00
+  })
+console.log("\nThe mild and cheap vacations are:", mildCheapSpots);
 
-// However, b/c it can't be reassigned doesn't mean it's not mutable
-const z = {
-  point: 3
-}
-z.point = 4 // realize that we can change the value inside the object
-console.log(z);
-
-// Note that we're still not allowed to reassign the object though
-z = {
-  point: 5
-}
+// Now imagine that our 401K is doing real well, and we can
+// visit manay places with our unlimited PTO,
+// let's find out the toal cost to visit all the warm countries
+const costOfWarmCountries = vacationSpots
+  .filter(function(spots){
+    return spots.climate === "Warm"
+  })
+  .map(function(spots){
+    return spots.cost
+  })
+  .reduce(function(prev, value){
+    return (prev || 0) + value
+  })
+  console.log("\nThe cost to visit the warm countries is:", costOfWarmCountries);
